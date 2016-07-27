0c77c7ec8b88dea1e6fa7203f764fcfc7528346a
Adrian Szulc <adrian.szulc@magently.com>
2016-07-27 16:27:23 +0200

    Setup new config page
    Change-Id: I259ff39928bc45a104c4b4f1d90eac4c2dd4366a

---
+++ Model/Config/Source/Feed/Attributes.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Doofinder\Feed\Model\Config\Source\Feed;
+
+class Attributes implements \Magento\Framework\Option\ArrayInterface
+{
+    public function toOptionArray()
+    {
+
+    }
+}
