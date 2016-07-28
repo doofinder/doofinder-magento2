1a4cb7117384b96690807569622a4db58a2c9347
Adrian Szulc <adrian.szulc@magently.com>
2016-07-28 16:37:22 +0200

    Setup source model for feed attributes
    Change-Id: I6638c464ff3a66a2e229b292e7855d5556138256

--- Model/Config/Source/Feed/Attributes.php
+++ Model/Config/Source/Feed/Attributes.php
@@ -4,8 +4,57 @@ namespace Doofinder\Feed\Model\Config\Source\Feed;
 
 class Attributes implements \Magento\Framework\Option\ArrayInterface
 {
+    protected $_scopeConfig;
+    protected $_eavConfig;
+    protected $_options;
+
+    public function __construct(
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \Magento\Eav\Model\Config $eavConfig
+    ) {
+        $this->_scopeConfig = $scopeConfig;
+        $this->_eavConfig = $eavConfig;
+    }
+
     public function toOptionArray()
     {
+        if (!$this->_options) {
+            $this->_options = array_merge(
+                $this->_getDoofinderDirectivesOptionArray(),
+                $this->_getCatalogProductAttributes());
+        }
 
+        return $this->_options;
+    }
+
+    protected function _getCatalogProductAttributes()
+    {
+        $attributes = array();
+
+        $productEntity = \Magento\Catalog\Model\Product::ENTITY;
+        $collection = $this->_eavConfig->getEntityType($productEntity)->getAttributeCollection();
+
+        foreach ($collection as $attribute) {
+            $code = $attribute->getAttributeCode();
+            $label = $attribute->getFrontendLabel();
+
+            $attributes[$code] = 'Attribute: ' . $code . ($label ? ' (' . $label . ')' : '');
+        }
+
+        return $attributes;
+    }
+
+    protected function _getDoofinderDirectivesOptionArray()
+    {
+        $options = array();
+
+        $storeScope = \Magento\Store\Model\ScopeInterface::SCOPE_STORE;
+        $directives = $this->_scopeConfig->getValue('directives', $storeScope);
+
+        foreach ($directives as $directive => $label) {
+            $options[$directive] = 'Doofinder: ' . $label;
+        }
+
+        return $options;
     }
 }
