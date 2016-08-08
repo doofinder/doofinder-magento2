189387c23ec6480374bf2295f9d3b182f0721232
Adrian Szulc <adrian.szulc@magently.com>
2016-08-08 15:19:11 +0200

    Setup helper for feed attributes
    Change-Id: I1080e13d39cbc357695d2f96f3dbb88e140a137d

---
+++ Helper/FeedConfig.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace Doofinder\Feed\Helper;
+
+/**
+ * Class FeedConfig
+ * @package Doofinder\Feed\Helper
+ */
+class FeedConfig extends \Magento\Framework\App\Helper\AbstractHelper
+{
+    /**
+     * @var array Feed attribute config
+     */
+    protected $_feedConfig;
+    /**
+     * @var \Magento\Framework\App\Config\ScopeConfigInterface
+     */
+    protected $_scopeConfig;
+
+    /**
+     * FeedConfig constructor.
+     *
+     * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+     */
+    public function __construct(
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+    ) {
+        $this->_scopeConfig = $scopeConfig;
+    }
+
+    /**
+     * Get feed attribute config.
+     *
+     * @return array
+     */
+    public function getFeedConfig()
+    {
+        if(!$this->_feedConfig) {
+            $this->_setFeedConfig();
+        }
+
+        return $this->_feedConfig;
+    }
+
+    /**
+     * Set feed config
+     */
+    protected function _setFeedConfig()
+    {
+        $this->_feedConfig = [
+            'data' => [
+                'config' => [
+                    'fetchers' => $this->_getFetchers(),
+                    'processors' => $this->_getProcessors()
+                ]
+            ]
+        ];
+    }
+
+    /**
+     * Setup fetchers.
+     *
+     * @return array
+     */
+    protected function _getFetchers()
+    {
+        return [
+            'Product' => []
+        ];
+    }
+
+    /**
+     * Setup processors.
+     *
+     * @return array
+     */
+    protected function _getProcessors()
+    {
+        return [
+            'Mapper' => $this->_getMapper(),
+            'Cleaner' => [],
+            'Xml' => []
+        ];
+    }
+
+    /**
+     * Setup feed mapper.
+     *
+     * @return array
+     */
+    protected function _getMapper()
+    {
+        return [
+            'map' => $this->_getFeedAttributes()
+        ];
+    }
+
+    /**
+     * Get feed attributes from config.
+     *
+     * @return array
+     */
+    protected function _getFeedAttributes()
+    {
+        $attributes = $this->_scopeConfig->getValue('doofinder_feed_feed/feed_attributes');
+
+        if(array_key_exists('additional_attributes', $attributes)) {
+            $additionalKeys = unserialize($attributes['additional_attributes']);
+            unset($attributes['additional_attributes']);
+
+            $additionalAttributes = array();
+            foreach($additionalKeys as $key) {
+                $additionalAttributes[$key['field']] = $key['additional_attribute'];
+            }
+
+            return array_merge($attributes, $additionalAttributes);
+        }
+
+        return $attributes;
+    }
+}
---
+++ Test/Unit/Helper/FeedConfigTest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Doofinder\Feed\Test\Unit\Helper\Data;
+
+/**
+ * Class DataTest
+ * @package Doofinder\Feed\Test\Unit\Helper\Data
+ */
+class FeedConfigTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \Magento\Framework\TestFramework\Unit\Helper\ObjectManager
+     */
+    protected $_objectManager;
+    /**
+     * @var \Magento\Framework\App\Config\ScopeConfigInterface
+     */
+    protected $_scopeInterfaceMock;
+    /**
+     * @var \Doofinder\Feed\Helper\Data
+     */
+    protected $_helper;
+
+    /**
+     * Prepares the environment before running a test.
+     */
+    public function setUp()
+    {
+        $this->_objectManager = new \Magento\Framework\TestFramework\Unit\Helper\ObjectManager($this);
+
+        $this->_scopeInterfaceMock = $this->getMock(
+            '\Magento\Framework\App\Config\ScopeConfigInterface',
+            [],
+            [],
+            '',
+            false
+        );
+
+        $this->_helper = $this->_objectManager->getObject(
+            '\Doofinder\Feed\Helper\FeedConfig',
+            [
+                'scopeConfig'   => $this->_scopeInterfaceMock,
+            ]
+        );
+    }
+
+    public function testGetFeedConfig()
+    {
+        $feedAttributes = [
+            'attr1' => 'value1',
+            'attr2' => 'value2'
+        ];
+
+        $this->_scopeInterfaceMock->expects($this->once())
+            ->method('getValue')
+            ->with('doofinder_feed_feed/feed_attributes')
+            ->will($this->returnValue($feedAttributes));
+
+        $expected = [
+            'data' => [
+                'config' => [
+                    'fetchers' => [
+                        'Product' => []
+                    ],
+                    'processors' => [
+                        'Mapper' => [
+                            'map' => $feedAttributes
+                        ],
+                        'Cleaner' => [],
+                        'Xml' => []
+                    ]
+                ]
+            ]
+        ];
+
+        $result = $this->_helper->getFeedConfig();
+
+        $this->assertSame($expected, $result);
+    }
+}
