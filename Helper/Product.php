1ff2b195ac89948c3c043564bc2f9ccd8da4dd52
Paweł Tatarczuk <pawel.tatarczuk@magently.com>
2016-08-09 09:40:26 +0200

    Add product helper
    Change-Id: I3889287fc16ac5e4c0e9b4bd4db8da5055294d15

---
+++ Helper/Product.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Doofinder\Feed\Helper;
+
+class Product extends \Magento\Framework\App\Helper\AbstractHelper
+{
+    /**
+     * @var \Magento\Catalog\Model\ResourceModel\Category\CollectionFactory
+     */
+    protected $_categoryCollectionFactory = null;
+
+    /**
+     * @var \Magento\Catalog\Helper\Image
+     */
+    protected $_imageHelper = null;
+
+    public function __construct(
+        \Magento\Catalog\Model\ResourceModel\Category\CollectionFactory $categoryCollectionFactory,
+        \Magento\Catalog\Helper\Image $imageHelper,
+        \Magento\Framework\App\Helper\Context $context
+    ) {
+        $this->_categoryCollectionFactory = $categoryCollectionFactory;
+        $this->_imageHelper = $imageHelper;
+        parent::__construct($context);
+    }
+
+    /**
+     * Get product url
+     *
+     * @todo Use store config
+     *
+     * @param \Magento\Catalog\Model\Product
+     * @return string
+     */
+    public function getProductUrl(\Magento\Catalog\Model\Product $product)
+    {
+        return $product->getProductUrl(false);
+    }
+
+    /**
+     * Get product categories
+     *
+     * @todo This might need some optimalization
+     *
+     * @param \Magento\Catalog\Model\Product
+     * @return \Magento\Catalog\Model\Category[][]
+     */
+    public function getProductCategoriesWithParents(\Magento\Catalog\Model\Product $product)
+    {
+        $categoryIds = $product->getResource()->getCategoryIds($product);
+
+        $categoryCollection = $this->_categoryCollectionFactory->create();
+        $categoryCollection
+            ->addIdFilter($categoryIds)
+            ->addAttributeToSelect('name')
+            ->load();
+
+        $categories = array();
+
+        foreach ($categoryCollection as $category) {
+            $categories[] = $category->getParentCategories();
+        }
+
+        return $categories;
+    }
+
+    /**
+     * Get product image url
+     *
+     * @todo Use store config
+     *
+     * @param \Magento\Catalog\Model\Product
+     * @return string|null
+     */
+    public function getProductImageUrl(\Magento\Catalog\Model\Product $product)
+    {
+        if ($product->hasImage()) {
+            return $this->_imageHelper
+                ->init($product, 'doofinder_image')
+                //->resize()
+                ->getUrl();
+        }
+    }
+
+    /**
+     * Get product price
+     *
+     * @todo This might not work properly with taxes
+     * @todo Add proper price rounding
+     *
+     * @param \Magento\Catalog\Model\Product
+     * @return float
+     */
+    public function getProductPrice(\Magento\Catalog\Model\Product $product)
+    {
+        return round($product->getPrice(), 2);
+    }
+}
---
+++ Test/Integration/Helper/ProductTest.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace Doofinder\Feed\Test\Integration\Helper;
+
+/**
+ * Test class for \Doofinder\Feed\Helper\Product
+ */
+class ProductTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \Doofinder\Feed\Helper\Product
+     */
+    private $_helper;
+
+    /**
+     * @var \Magento\Framework\ObjectManagerInterface
+     */
+    private $_objectManager;
+
+    /**
+     * @var \Magento\Catalog\Api\ProductRepositoryInterface
+     */
+    private $_productRepository;
+
+    /**
+     * @var \Magento\Catalog\Api\CategoryRepositoryInterface
+     */
+    private $_categoryRepository;
+
+    public function setUp()
+    {
+        $this->_objectManager = \Magento\TestFramework\Helper\Bootstrap::getObjectManager();
+
+        $this->_productRepository = $this->_objectManager
+            ->create('\Magento\Catalog\Api\ProductRepositoryInterface');
+
+        $this->_categoryRepository = $this->_objectManager
+            ->create('\Magento\Catalog\Api\CategoryRepositoryInterface');
+
+        $this->_helper = $this->_objectManager->create(
+            '\Doofinder\Feed\Helper\Product'
+        );
+    }
+
+    /**
+     * Test getProductUrl
+     *
+     * @magentoDataFixture Magento/Catalog/_files/product_simple.php
+     * @magentoAppIsolation enabled
+     */
+    public function testGetProductUrl()
+    {
+        $product = $this->_productRepository->get('simple');
+
+        $this->assertRegExp(
+            '/https?:\/\/[^\/]+(:\d+)?\/(.*\/)?simple-product\.html/',
+            $this->_helper->getProductUrl($product)
+        );
+    }
+
+    /**
+     * Test getProductCategoriesWithParents
+     *
+     * @magentoDataFixture Magento/Catalog/_files/categories.php
+     * @magentoAppIsolation enabled
+     */
+    public function testGetProductCategoriesWithParents()
+    {
+        $product = $this->_productRepository->get('12345');
+
+        $this->assertEquals(
+            [
+                [
+                    3 => '1/2/3',
+                    4 => '1/2/3/4',
+                ],
+                [
+                    3 => '1/2/3',
+                    4 => '1/2/3/4',
+                    5 => '1/2/3/4/5',
+                ],
+            ],
+            array_map(
+                function ($element) {
+                    return array_map(
+                        function ($category) {
+                            return $category->getPath();
+                        },
+                        $element
+                    );
+                },
+                $this->_helper->getProductCategoriesWithParents($product)
+            )
+        );
+    }
+
+    /**
+     * Test getProductImageUrl
+     *
+     * @magentoDataFixture Magento/Catalog/_files/product_with_image.php
+     * @magentoAppIsolation enabled
+     */
+    public function testGetProductImageUrl()
+    {
+        $product = $this->_productRepository->get('simple');
+
+        $this->assertRegExp(
+            // @codingStandardsIgnoreStart
+            '/https?:\/\/[^\/]+(:\d+)?\/(.*\/)?pub\/media\/catalog\/product\/cache\/\d+\/image\/[^\/]+\/\w\/\w\/magento_image\.jpg/',
+            // @codingStandardsIgnoreEnd
+            $this->_helper->getProductImageUrl($product)
+        );
+    }
+
+    /**
+     * Test getProductPrice
+     *
+     * @magentoDataFixture Magento/Catalog/_files/product_simple.php
+     * @magentoAppIsolation enabled
+     */
+    public function testGetProductPrice()
+    {
+        $product = $this->_productRepository->get('simple');
+
+        $this->assertEquals(
+            10,
+            $this->_helper->getProductPrice($product)
+        );
+    }
+}
