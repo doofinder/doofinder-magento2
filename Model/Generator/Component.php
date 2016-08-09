c76a0be141028c0c34c76ebfdf78252962e86af3
Paweł Tatarczuk <pawel.tatarczuk@magently.com>
2016-08-09 09:46:36 +0200

    Add logger to generator component
    Change-Id: I6e43ac1720d2a57f6ecc9a9b565b4ec22a56338b

--- Model/Generator/Component.php
+++ Model/Generator/Component.php
@@ -4,4 +4,16 @@ namespace Doofinder\Feed\Model\Generator;
 
 class Component extends \Magento\Framework\DataObject
 {
+    /**
+     * @var \Psr\Log\LoggerInterface
+     */
+    protected $_logger = null;
+
+    public function __construct(
+        \Psr\Log\LoggerInterface $logger,
+        array $data = []
+    ) {
+        $this->_logger = $logger;
+        parent::__construct($data);
+    }
 }
