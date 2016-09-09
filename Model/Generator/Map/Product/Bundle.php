f4e6c6f80fd775021f3bde74915ef98cc965b834
Paweł Tatarczuk <pawel.tatarczuk@magently.com>
2016-09-09 10:30:36 +0200

    Add maps for products of bundle and grouped types
    Change-Id: I8b6939deae1357c4181c46c183da8ff3c428d854

---
+++ Model/Generator/Map/Product/Bundle.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Doofinder\Feed\Model\Generator\Map\Product;
+
+use \Doofinder\Feed\Model\Generator\Map\Product;
+
+class Bundle extends Product
+{
+    /**
+     * Get bundle product price
+     *
+     * @param \Magento\Catalog\Model\Product $product
+     * @return float
+     */
+    protected function getProductPrice(\Magento\Catalog\Model\Product $product)
+    {
+        return $product->getPriceInfo()->getPrice('final_price')->getMinimalPrice()->getValue();
+    }
+}
