99691f633aad41bc032ad3330a380f0776e36818
Paweł Tatarczuk <pawel.tatarczuk@magently.com>
2016-10-28 10:41:57 +0200

    Fix issue with reschedule after feed config change
    Change-Id: I0d89a22dd1112d2900883d25e3cdeb15b0a49214

--- view/adminhtml/web/js/generateFeed.js
+++ view/adminhtml/web/js/generateFeed.js
@@ -4,29 +4,6 @@ define([
 ], function($, prototype) {
     var generate = function(config, node) {
 
-        var add_message = function(text) {
-            var html = '' +
-                '<div class="message-system-inner">' +
-                '    <ul class="message-system-list">' +
-                '        <li class="message message-notice">' + text + '</li>' +
-                '    </ul>' +
-                '</div>';
-
-            $('#system_messages').append(html);
-          };
-
-          document.observe("dom:loaded", function() {
-            if ($('doofinder_feed_feed_feed_cron')) {
-                var changed = false;
-                new Form.Observer('config-edit-form', 0.3, function(form, value) {
-                    if (changed) return;
-                    add_message('Configuration has changed. The feed generation will be rescheduled after saving.');
-                    form.insert('<input type="hidden" name="reset" value="1"/>');
-                    changed = true;
-                });
-            }
-        });
-
         node.on('click', function() {
             new Ajax.Request(config.ajaxUrl, {
                 onComplete: function(response) {
