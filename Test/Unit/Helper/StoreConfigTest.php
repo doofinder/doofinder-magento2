d16978cc69c8c57b40450f3cc809f98560fff664
Adrian Szulc <adrian.szulc@magently.com>
2018-03-16 14:55:26 +0100

    Change the logic of store getting
    refs #59541
    
    Change-Id: I3a71071d5f455b3bfa0c7c42e19a81347393befa

--- Test/Unit/Helper/StoreConfigTest.php
+++ Test/Unit/Helper/StoreConfigTest.php
@@ -15,7 +15,7 @@ class StoreConfigTest extends BaseTestCase
     private $scopeConfig;
 
     /**
-     * @var \Magento\Store\Model\StoreManagerInterface
+     * @var \Magento\Store\Model\StoreManager
      */
     private $storeManager;
 
@@ -28,6 +28,11 @@ class StoreConfigTest extends BaseTestCase
      * @var \Magento\Store\Api\Data\StoreInterface
      */
     private $storeInterface;
+
+    /**
+     * @var \Magento\Framework\App\Request\Http
+     */
+    private $request;
 
     /**
      * @var \Doofinder\Feed\Helper\StoreConfig
@@ -52,7 +57,7 @@ class StoreConfigTest extends BaseTestCase
         );
 
         $this->storeManager = $this->getMock(
-            \Magento\Store\Model\StoreManagerInterface::class,
+            \Magento\Store\Model\StoreManager::class,
             [],
             [],
             '',
@@ -75,12 +80,21 @@ class StoreConfigTest extends BaseTestCase
             false
         );
 
+        $this->request = $this->getMock(
+            \Magento\Framework\App\Request\Http::class,
+            [],
+            [],
+            '',
+            false
+        );
+
         $this->helper = $this->objectManager->getObject(
             \Doofinder\Feed\Helper\StoreConfig::class,
             [
                 'scopeConfig'  => $this->scopeConfig,
                 'storeManager'    => $this->storeManager,
                 'logger'        => $this->logger,
+                '_request'   => $this->request,
             ]
         );
     }
@@ -151,17 +165,16 @@ class StoreConfigTest extends BaseTestCase
     }
 
     /**
-     * Test getStoreCodes() method
+     * Test getStoreCodes by store id
      *
-     * @param  boolean $onlyActive
-     * @param  string $current
-     * @param  array $stores
-     * @param  array $expected
      * @return void
-     * @dataProvider providerTestGetStoreCodes
      */
-    public function testGetStoreCodes($onlyActive, $current, array $stores, array $expected)
+    public function testGetStoreCodesByStoreId()
     {
+        $storeId = 1;
+        $storeCode = 'store_code';
+        $this->request->method('getParam')->with('store')->willReturn($storeId);
+
         $store = $this->getMock(
             \Magento\Store\Model\Store::class,
             [],
@@ -169,22 +182,73 @@ class StoreConfigTest extends BaseTestCase
             '',
             false
         );
-        $store->method('getCode')->willReturn($current);
-        $this->storeManager->method('getStore')->willReturn($store);
+        $store->method('getCode')->willReturn($storeCode);
+
+        $this->storeManager->method('getStore')->with($storeId)->willReturn($store);
+        $this->assertEquals([$storeCode], $this->helper->getStoreCodes());
+    }
+
+    /**
+     * Test getStoreCodes by website id
+     *
+     * @return void
+     */
+    public function testGetStoreCodesByWebsiteId()
+    {
+        $expected = ['store_one', 'store_two'];
+
+        $storeOne = $this->getMock(
+            \Magento\Store\Model\Store::class,
+            [],
+            [],
+            '',
+            false
+        );
+        $storeOne->method('getCode')->willReturn('store_one');
+        $storeOne->method('isActive')->willReturn(true);
+
+        $storeTwo = $this->getMock(
+            \Magento\Store\Model\Store::class,
+            [],
+            [],
+            '',
+            false
+        );
+        $storeTwo->method('getCode')->willReturn('store_two');
+        $storeTwo->method('isActive')->willReturn(true);
+
+        $this->request->expects($this->at(1))->method('getParam')->with('website')->willReturn(1);
+        $this->storeManager->method('getStoreByWebsiteId')->with(1)->willReturn([1, 2]);
+        $this->storeManager->expects($this->at(1))->method('getStore')->with(1)->willReturn($storeOne);
+        $this->storeManager->expects($this->at(2))->method('getStore')->with(2)->willReturn($storeTwo);
+
+        $this->assertEquals($expected, $this->helper->getStoreCodes());
+    }
+
+    /**
+     * Test getStoreCodes without specified website or store
+     * @param boolean $onlyActive
+     * @param array $stores
+     * @param array $expected
+     * @return void
+     * @dataProvider providerTestGetStoreCodes
+     */
+    public function testGetStoreCodes($onlyActive, array $stores, array $expected)
+    {
+        $this->request->method('getParam')->willReturn(null);
         $this->storeManager->method('getStores')->willReturn($stores);
 
         $this->assertEquals($expected, $this->helper->getStoreCodes($onlyActive));
     }
 
     /**
-     * Data provider for testGetStoresCodes() test
+     * Data provider for testGetStoreCodes()
      *
      * @return array
      */
     public function providerTestGetStoreCodes()
     {
         $stores = [];
-
         $store = $this->getMock(
             \Magento\Store\Model\Store::class,
             [],
@@ -192,8 +256,8 @@ class StoreConfigTest extends BaseTestCase
             '',
             false
         );
-        $store->method('isActive')->willReturn(true);
         $store->method('getCode')->willReturn('active');
+        $store->method('isActive')->willReturn(true);
         $stores[] = $store;
 
         $store = $this->getMock(
@@ -203,14 +267,13 @@ class StoreConfigTest extends BaseTestCase
             '',
             false
         );
-        $store->method('isActive')->willReturn(false);
         $store->method('getCode')->willReturn('inactive');
+        $store->method('isActive')->willReturn(false);
         $stores[] = $store;
 
         return [
-            [true, 'admin', $stores, ['active']],
-            [false, 'admin', $stores, ['active', 'inactive']],
-            [true, 'sample', $stores, ['sample']],
+            [true, $stores, ['active']],
+            [false, $stores, ['active', 'inactive']]
         ];
     }
 
