77c75b3eeba666b67e9c4fad5382ea6d50b3cc19
Roman Ludwicki <roman.ludwicki@magently.com>
2019-06-10 11:22:22 +0200

    Include store code to product URLs
    refs #62463
    
    Change-Id: I88f9732750f54ec451389c60dcbd30f2c5cfaf58

---
+++ Plugin/Url/RouteParamsResolver.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Doofinder\Feed\Plugin\Url;
+
+/**
+ * Plugin for \Magento\Framework\Url\RouteParamsResolver
+ *
+ * Fix for Magento\Store\Url\Plugin\RouteParamsResolver for Magento 2.1.x
+ * where '___store' is adding to query params due to incorrect condition
+ */
+class RouteParamsResolver
+{
+    /**
+     * @var \Magento\Framework\Url\QueryParamsResolverInterface
+     */
+    private $queryParamsResolver;
+
+    /**
+     * Initialize dependencies.
+     *
+     * @param \Magento\Framework\Url\QueryParamsResolverInterface $queryParamsResolver
+     */
+    public function __construct(
+        \Magento\Framework\Url\QueryParamsResolverInterface $queryParamsResolver
+    ) {
+        $this->queryParamsResolver = $queryParamsResolver;
+    }
+
+    /**
+     * Process scope query parameters.
+     *
+     * @param \Magento\Framework\Url\RouteParamsResolver $subject
+     * @param array $data
+     * @param boolean $unsetOldParams
+     * @throws \Magento\Framework\Exception\NoSuchEntityException
+     *
+     * @return array
+     *
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    // @codingStandardsIgnoreLine
+    public function beforeSetRouteParams(
+        \Magento\Framework\Url\RouteParamsResolver $subject,
+        array $data,
+        $unsetOldParams = true
+    ) {
+        if (isset($data['doofinder_product_url'])) {
+            $params = $this->queryParamsResolver->getQueryParams();
+            if (isset($params['___store'])) {
+                unset($params['___store']);
+                $this->queryParamsResolver->setQueryParams($params);
+            }
+
+            unset($data['doofinder_product_url']);
+        }
+
+        return [$data, $unsetOldParams];
+    }
+}
