be7ab08874937e3c27ceab42e8ad7463be0906ff
Carlos Escribano <carlos@doofinder.com>
2020-05-06 13:53:05 +0200

    fix: remove require (typo)

--- Helper/ComposerVersionProvider.php
+++ Helper/ComposerVersionProvider.php
@@ -63,7 +63,7 @@ class ComposerVersionProvider
      */
     private function getVendorPath()
     {
-        return require VENDOR_PATH; //phpcs:ignore -- constant leads to executable php file
+        return VENDOR_PATH; //phpcs:ignore -- constant leads to executable php file
     }
 
     /**
