a81dff1589e0437655d0ab1ff9d7c3cdb39c17f6
sofia-doofinder <92720455+sofia-doofinder@users.noreply.github.com>
2022-08-18 12:43:04 +0200

    Store hashid after store creation (#203)

--- Controller/Adminhtml/Integration/CreateStore.php
+++ Controller/Adminhtml/Integration/CreateStore.php
@@ -99,17 +99,19 @@ class CreateStore extends Action implements HttpGetActionInterface
         $success = true;
         foreach($this->storeConfig->getAllWebsites() as $website) {
             try {
+                $searchEngineData = $this->generateSearchEngineData((int)$website->getId());
                 $websiteConfig = [
                     "name" => $website->getName(),
                     "platform" => "magento2",
                     "primary_language" => $this->storeConfig->getLanguageFromStore($website->getDefaultStore()),
                     "skip_indexation" => false,
                     "sector" => $this->storeConfig->getValueFromConfig(StoreConfig::SECTOR_VALUE_CONFIG),
-                    "search_engines" => $this->generateSearchEngineData((int)$website->getId()),
+                    "search_engines" => $searchEngineData["searchEngineConfig"],
                     "query_input" => "#search"
                 ];
                 $response = $this->storeConfig->createStore($websiteConfig);
                 $this->saveInstallationConfig((int)$website->getId(), $response["installation_id"], $response["script"]);
+                $this->saveSearchEngineConfig($searchEngineData["storesConfig"], $response["search_engines"]);
                 $this->setCustomAttributes();
                 $this->cleanCache();
         
@@ -124,15 +126,17 @@ class CreateStore extends Action implements HttpGetActionInterface
     public function generateSearchEngineData($websiteID)
     {
         $searchEngineConfig = [];
+        $storesConfig = [];
+
         foreach ($this->storeConfig->getWebsiteStores($websiteID) as $store) {
-            $integrationToken = $this->integrationService
-                ->get($this->storeConfig->getIntegrationId())
-                ->getData(Tokens::DATA_TOKEN);
+            $integrationToken = $this->integrationService->get($this->storeConfig->getIntegrationId())->getData(Tokens::DATA_TOKEN);
+            $language = $this->storeConfig->getLanguageFromStore($store);
+            $currency = strtoupper($store->getCurrentCurrency()->getCode());
 
             $searchEngineConfig[] = [
                 "name" => $store->getName(),
-                "language" => $this->storeConfig->getLanguageFromStore($store),
-                "currency" => strtoupper($store->getCurrentCurrency()->getCode()),
+                "language" => $language,
+                "currency" => $currency,
                 "site_url" => $store->getBaseUrl(),
                 "datatypes" => [
                     [
@@ -151,14 +155,38 @@ class CreateStore extends Action implements HttpGetActionInterface
                     ]
                 ]
             ];
+
+            $storesConfig[$language][$currency] = $store->getId();
         }
-        return $searchEngineConfig;
+        return ["searchEngineConfig" => $searchEngineConfig, "storesConfig" => $storesConfig];
     }
 
+    /**
+     * Function to store into the data base the installation id as well as the layer script
+     */
     private function saveInstallationConfig($websiteID, $installationId, $script) 
     {
         $this->configWriter->save(StoreConfig::DISPLAY_LAYER_INSTALLATION_ID, $installationId, ScopeInterface::SCOPE_WEBSITES, $websiteID);
         $this->configWriter->save(StoreConfig::DISPLAY_LAYER_SCRIPT_CONFIG, $script, ScopeInterface::SCOPE_WEBSITES, $websiteID);
+    }
+
+    /**
+     * Function to store into data base the relation of each store view his hashid related
+     * The storeConfig variable has the following format:
+     * {"en":{"USD":"1"},"de":{"USD":"2"}}
+     * The searchEngines variable has the following format:
+     * "search_engines":{"de":{"USD":"024d8eb1caa649775d08f3f69ddf333a"},"en":{"USD":"c3981a773ac987e5828c94677cda237f"}}
+     * We're going to iterate over the search_engines because there is the data created in doofinder. May occour that some
+     * of the data that we've in storeConfig has some invalid parameter and will be bypass during the creation.
+     */
+    private function saveSearchEngineConfig($storesConfig, $searchEngines)
+    {
+        foreach ($searchEngines as $language => $values) {
+            foreach ($values as $currency => $hashid) {
+                $storeId = $storesConfig[$language][$currency];
+                $this->configWriter->save(StoreConfig::HASH_ID_CONFIG, $hashid, ScopeInterface::SCOPE_STORES, $storeId);
+            }
+        }
     }
 
     /**
