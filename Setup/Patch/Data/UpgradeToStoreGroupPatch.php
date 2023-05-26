e1c1a96075abae9d7c56f0c2b29d136c98e00536
Pedro Rivas <pedro.rivas@doofinder.com>
2023-05-26 15:28:57 +0200

    Fix migration

--- Setup/Patch/Data/UpgradeToStoreGroupPatch.php
+++ Setup/Patch/Data/UpgradeToStoreGroupPatch.php
@@ -6,6 +6,7 @@ use Magento\Framework\Setup\ModuleDataSetupInterface;
 use Magento\Framework\Setup\Patch\DataPatchInterface;
 use Magento\Framework\Setup\Patch\PatchVersionInterface;
 use Magento\Store\Model\ScopeInterface;
+use Magento\Store\Model\ResourceModel\Group\CollectionFactory as GroupCollectionFactory;
 use Magento\Store\Model\StoreManagerInterface;
 use Magento\Framework\App\Config\Storage\WriterInterface;
 
@@ -15,47 +16,44 @@ class UpgradeToStoreGroupPatch implements DataPatchInterface, PatchVersionInterf
     private $storeManager;
     private $configWriter;
     private $scopeConfig;
-    private $logger;
+    private $groupCollectionFactory;
 
     public function __construct(
         ModuleDataSetupInterface $moduleDataSetup,
         StoreManagerInterface $storeManager,
         WriterInterface $configWriter,
         \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        GroupCollectionFactory $groupCollectionFactory
     ) {
-        $this->logger->debug("Entra en el construct");
         $this->moduleDataSetup = $moduleDataSetup;
         $this->storeManager = $storeManager;
         $this->configWriter = $configWriter;
         $this->scopeConfig = $scopeConfig;
+        $this->groupCollectionFactory = $groupCollectionFactory;
     }
 
     public function apply()
     {
         $this->moduleDataSetup->startSetup();
 
-        $websites = $this->storeManager->getWebsites();
+        $groupCollection = $this->groupCollectionFactory->create();
+        $scriptIdPath = 'doofinder_config_config/doofinder_layer/script';
+        $installationIdPath = 'doofinder_config_config/doofinder_layer/installation_id';
 
-        foreach ($websites as $website) {
-            $groups = $website->getGroups();
-            
-            foreach ($groups as $group) {
-                $configPathScript = 'doofinder_config_config/doofinder_layer/script';
-                $configPathInstallationId = 'doofinder_config_config/doofinder_layer/installation_id';
-                
-                if(!empty($script)){
-                    $this->configWriter->save($scriptIdPath, $script, ScopeInterface::SCOPE_GROUP, $group->getId());
-                    $this->configWriter->delete($scriptIdPath, ScopeInterface::SCOPE_WEBSITES, $websiteId);
-                }
-    
-                $installationId = $this->scopeConfig->getValue($installationIdPath, ScopeInterface::SCOPE_WEBSITES, $websiteId);
-                if(!empty($installationId)){
-                    $this->configWriter->save($installationIdPath, $installationId, ScopeInterface::SCOPE_GROUP, $group->getId());
-                    $this->configWriter->delete($installationIdPath, ScopeInterface::SCOPE_WEBSITES, $websiteId);
-                }
+        foreach ($groupCollection as $group) {
+            $websiteId = $group->getWebsiteId();
+            $script = $this->scopeConfig->getValue($scriptIdPath, ScopeInterface::SCOPE_WEBSITES, $websiteId);
+            if(!empty($script)) {
+                $this->configWriter->save($scriptIdPath, $script, ScopeInterface::SCOPE_GROUP, $group->getId());
+                $this->configWriter->delete($scriptIdPath, ScopeInterface::SCOPE_WEBSITES, $websiteId);
+            }
+
+            $installationId = $this->scopeConfig->getValue($installationIdPath, ScopeInterface::SCOPE_WEBSITES, $websiteId);
+            if(!empty($installationId)) {
+                $this->configWriter->save($installationIdPath, $installationId, ScopeInterface::SCOPE_GROUP, $group->getId());
+                $this->configWriter->delete($installationIdPath, ScopeInterface::SCOPE_WEBSITES, $websiteId);
             }
         }
-
         $this->moduleDataSetup->endSetup();
     }
 
@@ -71,6 +69,6 @@ class UpgradeToStoreGroupPatch implements DataPatchInterface, PatchVersionInterf
 
     public static function getVersion()
     {
-        return '0.11.0';
+        return '0.11.1';
     }
 }
