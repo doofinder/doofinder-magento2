8352f20576c95fbf628b6c969eeb7b3738eeb6a2
brunovesar <158461069+brunovesar@users.noreply.github.com>
2024-03-11 09:33:55 +0100

    Clarify update on save and unify options with other platforms (#304)

--- Model/Config/Source/Cronexpression.php
+++ Model/Config/Source/Cronexpression.php
@@ -15,12 +15,15 @@ class Cronexpression implements OptionSourceInterface
     public function toOptionArray(): array
     {
         return [
-            ['value' => '', 'label' => 'Everyday'],
-            ['value' => '*/5 * * * *', 'label' => '5 Minutes'],
-            ['value' => '*/10 * * * *', 'label' => '10 Minutes'],
-            ['value' => '*/15 * * * *', 'label' => '15 Minutes'],
-            ['value' => '*/30 * * * *', 'label' => '30 Minutes'],
-            ['value' => '0 * * * *', 'label' => '60 Minutes'],
+            ['value' => '', 'label' => 'Disabled'],
+            ['value' => '*/5 * * * *', 'label' => 'Each 5 Minutes'],
+            ['value' => '*/15 * * * *', 'label' => 'Each 15 Minutes'],
+            ['value' => '*/30 * * * *', 'label' => 'Each 30 Minutes'],
+            ['value' => '0 */1 * * *', 'label' => 'Each hour'],
+            ['value' => '0 */2 * * *', 'label' => 'Each 2 hours'],
+            ['value' => '0 */6 * * *', 'label' => 'Each 6 hours'],
+            ['value' => '0 */12 * * *', 'label' => 'Each 12 hours'],
+            ['value' => '0 0 * * *', 'label' => 'Once a day'],
         ];
     }
 }
