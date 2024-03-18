0cc1580d01b6a1967c436ddc7c786862859b9179
David Molina Cano <128705267+davidmolinacano@users.noreply.github.com>
2024-03-18 13:31:14 +0100

    Managed DeferredPromise from the Live Layer (#308)

--- view/frontend/web/js/df_add_to_cart.js
+++ view/frontend/web/js/df_add_to_cart.js
@@ -5,12 +5,20 @@ require(["jquery", "mage/url", "Magento_Customer/js/customer-data"], function (
 ) {
   $(document).ready(function () {
     document.addEventListener("doofinder.cart.add", function (event) {
-      const { item_id, amount } = event.detail;
-      addProductToCart(item_id, amount);
+      const { item_id, amount, statusPromise } = event.detail;
+      addProductToCart(item_id, amount, statusPromise);
     });
   });
 
-  function addProductToCart(item_id, amount) {
+  class DoofinderAddToCartError extends Error {
+    constructor(reason, status = "") {
+      const message = "Error adding an item to the cart. Reason: " + reason + ". Status code: " + status;
+      super(message);
+      this.name = "DoofinderAddToCartError";
+    }
+  } 
+
+  function addProductToCart(item_id, amount, statusPromise) {
     amount = !amount ? 1 : parseInt(amount);
     item_id = parseInt(item_id);
     /*
@@ -28,13 +36,18 @@ require(["jquery", "mage/url", "Magento_Customer/js/customer-data"], function (
       url: urlBuilder.build("doofinderfeed/Product/AddToCart"),
       success: function (data) {
         if (data.hasOwnProperty("product_url")) {
+          statusPromise.reject(new DoofinderAddToCartError("Configurable product. Redirecting to product URL", 200));
           window.location = data.product_url;
           return;          
         }
+        statusPromise.resolve("The item has been successfully added to the cart.");
       },
       complete: function () {
         customerData.invalidate(["cart"]);
         customerData.reload(["cart"], true);
+      },
+      error: function(xhr, ajaxOptions, thrownError) {
+        statusPromise.reject(new DoofinderAddToCartError(thrownError, xhr.status));
       },
       showLoader: true,
     });
