1c5c2e70ec00ee73dda48d8bcffae9a6cfbf1f85
Sofia Moreno Oñate <sofia@doofinder.com>
2024-11-13 18:40:34 +0100

    Fix issue with fixtures

--- Cron/Processor.php
+++ Cron/Processor.php
@@ -1,24 +1,9 @@
 <?php
-/**
- * By removing the UNIQUE restriction from our table, it happens that during imports or product generation with fixtures, 
- * duplicate elements are inserted into the table. These operations are handled within transactions and it seems that our code 
- * is currently not able to determine if the element is already added when it is within a transaction.
- * 
- * As a result, when executing the Processor for the Update on Save, we receive more elements than we actually need to process, 
- * because many of them are duplicates.
- * 
- * So what we do here is clone the original collection that has all the ungrouped elements. 
- * Then, a group is applied to the copy and thus we ensure that we process unique elements. 
- * At the end, the elements of the original collection are deleted from the table.
- * 
- * This logic is applied in the same way in the others two functions updateItems and deleteItems.
- */
 
 declare(strict_types=1);
 
 namespace Doofinder\Feed\Cron;
 
-use Doofinder\Feed\Api\Data\ChangedItemInterface;
 use Doofinder\Feed\Helper\Item as ItemHelper;
 use Doofinder\Feed\Helper\StoreConfig;
 use Doofinder\Feed\Model\ChangedItem\DocumentsProvider;
@@ -135,13 +120,8 @@ class Processor
     private function createItems($store, $itemType, $indice)
     {
         $collection = $this->changedItemCollectionFactory->create()->filterCreated((int)$store->getId(), $itemType);
-        
-        // Avoid process more than once the same product due to Magento2 product import process.
-        $groupedCollection = clone $collection;
-        $groupedCollection->getSelect()->group(ChangedItemInterface::ITEM_ID);
-
-        if ($groupedCollection->getSize()) {
-            $created = $this->documentsProvider->getBatched($groupedCollection, (int)$store->getId());
+        if ($collection->getSize()) {
+            $created = $this->documentsProvider->getBatched($collection, (int)$store->getId());
             foreach ($this->batch->getItems($created, $this->batchSize) as $batchDocuments) {
                 $items = $this->mapItems($batchDocuments);
                 if (count($items)) {
@@ -173,13 +153,8 @@ class Processor
     private function updateItems($store, $itemType, $indice)
     {
         $collection = $this->changedItemCollectionFactory->create()->filterUpdated((int)$store->getId(), $itemType);
-
-        // Avoid process more than once the same product due to Magento2 product import process.
-        $groupedCollection = clone $collection;
-        $groupedCollection->getSelect()->group(ChangedItemInterface::ITEM_ID);
-
-        if ($groupedCollection->getSize()) {
-            $updated = $this->documentsProvider->getBatched($groupedCollection, (int)$store->getId());
+        if ($collection->getSize()) {
+            $updated = $this->documentsProvider->getBatched($collection, (int)$store->getId());
             foreach ($this->batch->getItems($updated, $this->batchSize) as $batchDocuments) {
                 $items = $this->mapItems($batchDocuments);
                 if (count($items)) {
@@ -211,13 +186,8 @@ class Processor
     private function deleteItems($store, $itemType, $indice)
     {
         $collection = $this->changedItemCollectionFactory->create()->filterDeleted((int)$store->getId(), $itemType);
-
-        // Avoid process more than once the same product due to Magento2 product import process.
-        $groupedCollection = clone $collection;
-        $groupedCollection->getSelect()->group(ChangedItemInterface::ITEM_ID);
-
-        if ($groupedCollection->getSize()) {
-            $deleted = $this->documentsProvider->getBatched($groupedCollection);
+        if ($collection->getSize()) {
+            $deleted = $this->documentsProvider->getBatched($collection);
             foreach ($this->batch->getItems($deleted, $this->batchSize) as $batchDeleted) {
                 $items = $this->mapItems($batchDeleted);
                 if (count($items)) {
