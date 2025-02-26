b6d8aadafb73577b659128dca9e0144b66021945
Alberto Mur <alberto.mur@doofinder.com>
2025-02-26 16:13:04 +0100

    Organize module source into `Doofinder/Feed`

---
+++ Doofinder/Feed/ApiClient/Client.php
@@ -0,0 +1,305 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Doofinder\Feed\ApiClient;
+
+use Doofinder\Feed\Errors\ApiClient\InvalidApiKey;
+use Doofinder\Feed\Errors\BadRequest;
+use Doofinder\Feed\Errors\IndexingInProgress;
+use Doofinder\Feed\Errors\NotAllowed;
+use Doofinder\Feed\Errors\NotFound;
+use Doofinder\Feed\Errors\QuotaExhausted;
+use Doofinder\Feed\Errors\ThrottledResponse;
+use Doofinder\Feed\Errors\TypeAlreadyExists;
+use Doofinder\Feed\Errors\Utils;
+use Doofinder\Feed\Errors\WrongResponse;
+use Doofinder\Feed\Helper\StoreConfig;
+use GuzzleHttp\Client as GuzzleClient;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Exception\TransferException;
+use GuzzleHttp\RequestOptions;
+use Psr\Http\Message\ResponseInterface;
+
+class Client
+{
+    /** Management API type */
+    public const MANAGEMENT_API = 'api';
+
+    /** Search API type */
+    public const SEARCH_API = 'search';
+
+    /** Dooplugins type */
+    public const DOOPLUGINS = 'dooplugins';
+
+    /** @var StoreConfig */
+    private $storeConfig;
+
+    /** @var string  */
+    private $apiType;
+
+    /** @var string */
+    private $apiKey;
+
+    /** @var string */
+    private $token;
+
+    /** @var string */
+    private $clusterRegion;
+
+    /** @var GuzzleClient  */
+    private $guzzleClient;
+
+    /** @var ResponseInterface  */
+    private $response;
+
+    /**
+     * @param StoreConfig $storeConfig
+     * @param GuzzleClient $guzzleClient
+     * @param string $apiType
+     * @param string|null $apiKey
+     * @throws InvalidApiKey
+     */
+    public function __construct(
+        StoreConfig $storeConfig,
+        GuzzleClient $guzzleClient,
+        string $apiKey = null,
+        string $apiType = self::MANAGEMENT_API
+    ) {
+        $this->storeConfig  = $storeConfig;
+        $this->guzzleClient = $guzzleClient;
+        $this->apiType      = $apiType;
+        $this->setApiTokenRegion($apiKey);
+    }
+
+    /**
+     * Request with GET verb
+     *
+     * @param string $path
+     * @return string
+     * @throws BadRequest
+     * @throws IndexingInProgress
+     * @throws NotAllowed
+     * @throws NotFound
+     * @throws QuotaExhausted
+     * @throws ThrottledResponse
+     * @throws TypeAlreadyExists
+     * @throws WrongResponse
+     */
+    public function get(string $path): string
+    {
+        try {
+            $this->response = $this->guzzleClient->get(
+                $this->getApiBaseURL() . $path,
+                [
+                    RequestOptions::VERIFY  => false,
+                    RequestOptions::HEADERS => $this->getHeaders(),
+                    RequestOptions::EXPECT  => '',
+                ]
+            );
+        } catch (GuzzleException $e) {
+            $errorResponse = $this->parseErrorResponse($e->getMessage());
+            Utils::handleErrors($e->getCode(), $errorResponse);
+            throw new BadRequest($errorResponse);
+        }
+
+        return $this->getResult();
+    }
+
+    /**
+     * Request with POST verb
+     *
+     * @param string $path
+     * @param array $body
+     * @return string
+     * @throws BadRequest
+     * @throws IndexingInProgress
+     * @throws NotAllowed
+     * @throws NotFound
+     * @throws QuotaExhausted
+     * @throws ThrottledResponse
+     * @throws TypeAlreadyExists
+     * @throws WrongResponse
+     */
+    public function post(string $path, array $body): string
+    {
+        try {
+            $this->response = $this->guzzleClient->post(
+                $this->getApiBaseURL() . $path,
+                [
+                    RequestOptions::VERIFY  => false,
+                    RequestOptions::HEADERS => $this->getHeaders(),
+                    RequestOptions::JSON    => $body,
+                    RequestOptions::EXPECT  => '',
+                ]
+            );
+        } catch (GuzzleException $e) {
+            $errorResponse = $this->parseErrorResponse($e->getMessage());
+            Utils::handleErrors($e->getCode(), $errorResponse);
+            throw new BadRequest($errorResponse);
+        }
+
+        return $this->getResult();
+    }
+
+    /**
+     * Request with PATCH verb
+     *
+     * @param string $path
+     * @param array $body
+     * @return string
+     * @throws BadRequest
+     * @throws IndexingInProgress
+     * @throws NotAllowed
+     * @throws NotFound
+     * @throws QuotaExhausted
+     * @throws ThrottledResponse
+     * @throws TypeAlreadyExists
+     * @throws WrongResponse
+     */
+    public function patch(string $path, array $body): string
+    {
+        try {
+            $this->response = $this->guzzleClient->patch(
+                $this->getApiBaseURL() . $path,
+                [
+                    RequestOptions::VERIFY  => false,
+                    RequestOptions::HEADERS => $this->getHeaders(),
+                    RequestOptions::JSON    => $body,
+                    RequestOptions::EXPECT  => '',
+                ]
+            );
+        } catch (GuzzleException $e) {
+            $errorResponse = $this->parseErrorResponse($e->getMessage());
+            Utils::handleErrors($e->getCode(), $errorResponse);
+            throw new BadRequest($errorResponse);
+        }
+
+        return $this->getResult();
+    }
+
+    /**
+     * Request with DELETE verb
+     *
+     * @param string $path
+     * @param array $body
+     * @return string
+     * @throws BadRequest
+     * @throws IndexingInProgress
+     * @throws NotAllowed
+     * @throws NotFound
+     * @throws QuotaExhausted
+     * @throws ThrottledResponse
+     * @throws TypeAlreadyExists
+     * @throws WrongResponse
+     */
+    public function delete(string $path, array $body): string
+    {
+        try {
+            $this->response = $this->guzzleClient->delete(
+                $this->getApiBaseURL() . $path,
+                [
+                    RequestOptions::VERIFY  => false,
+                    RequestOptions::HEADERS => $this->getHeaders(),
+                    RequestOptions::JSON    => $this->flattenArray($body),
+                    RequestOptions::EXPECT  => '',
+                ]
+            );
+        } catch (GuzzleException $e) {
+            $errorResponse = $e->getResponse()->getBody()->getContents();
+            Utils::handleErrors($e->getCode(), $errorResponse);
+            throw new BadRequest($errorResponse);
+        }
+
+        return $this->getResult();
+    }
+
+    /**
+     * Gets results handling errors
+     *
+     * @return string
+     * @throws BadRequest
+     * @throws IndexingInProgress
+     * @throws NotAllowed
+     * @throws NotFound
+     * @throws QuotaExhausted
+     * @throws ThrottledResponse
+     * @throws TypeAlreadyExists
+     * @throws WrongResponse
+     */
+    private function getResult(): string
+    {
+        if (!$this->response) {
+            throw new TransferException();
+        }
+        $statusCode      = (int)$this->response->getStatusCode();
+        $contentResponse = $this->response->getBody()->getContents();
+        Utils::handleErrors($statusCode, $contentResponse);
+
+        return $contentResponse;
+    }
+
+    /**
+     * Gets necessary headers for the requests
+     *
+     * @return string[]
+     */
+    private function getHeaders(): array
+    {
+        return [
+            'Authorization' => 'Token ' . $this->token,
+        ];
+    }
+
+    /**
+     * Sets API token region in order to match the corresponding one
+     *
+     * @param string|null $apiKey
+     * @throws InvalidApiKey
+     */
+    private function setApiTokenRegion(?string $apiKey)
+    {
+        $this->apiKey = $apiKey ?? $this->storeConfig->getApiKey() ?? '';
+        $clusterToken = explode('-', str_replace('https://', '', $this->apiKey));
+        if (count($clusterToken) != 2) {
+            throw new InvalidApiKey("Invalid API Key provided");
+        }
+        $this->clusterRegion = $clusterToken[0];
+        $this->token = $clusterToken[1];
+    }
+
+    /**
+     * @return string
+     */
+    private function getApiBaseURL(): string
+    {
+        if ($this->apiType === self::DOOPLUGINS) {
+            return sprintf("https://%s-plugins.doofinder.com", $this->clusterRegion);
+        }
+        return sprintf("https://%s-%s.doofinder.com", $this->clusterRegion, $this->apiType);
+    }
+
+    /**
+     * @param string $message
+     * @return string
+     */
+    private function parseErrorResponse(string $message): string
+    {
+        $arr = preg_split('/\r\n|\r|\n/', $message);
+
+        return $arr[1] ?? $message;
+    }
+
+    /**
+     * @param array $ids
+     * @return array
+     */
+    private function flattenArray(array $ids): array
+    {
+        $flattened_ids = [];
+        array_walk_recursive($ids, function ($id) use (&$flattened_ids) {
+             $flattened_ids[] = $id;
+        });
+        return $flattened_ids;
+    }
+}
