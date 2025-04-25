a75de34b4cfcaf36e3f8def124e6ac2e99a45f42
David Molina Cano <128705267+davidmolinacano@users.noreply.github.com>
2025-04-25 09:13:01 +0200

    PLUGINRANGERS-285 | Fixed more warnings according to PHPCS, part 5 (#399)
    * feat: Fixed more warnings according to PHPCS
    
    * feat: Fixed more files
    
    * feat: Fixed more warnings according to PHPCS, part 3
    
    * feat: Avoided failures in Swagger
    
    * fix: Fixed warnings in more files
    
    * fix: Fixed warnings in even more files

--- Doofinder/Feed/Block/System/Config/CleanIntegration.php
+++ Doofinder/Feed/Block/System/Config/CleanIntegration.php
@@ -8,31 +8,52 @@ use Magento\Framework\Data\Form\Element\AbstractElement;
 
 class CleanIntegration extends Field
 {
+    /**
+     * Template to be used
+     *
+     * @var string
+     */
     protected $_template = 'Doofinder_Feed::System/Config/cleanIntegrationButton.phtml';
-    public function __construct(Context $context, array $data = [])
-    {
-        parent::__construct($context, $data);
-    }
 
+    /**
+     * @inheritdoc
+     */
     public function render(AbstractElement $element)
     {
         $element->unsScope()->unsCanUseWebsiteValue()->unsCanUseDefaultValue();
         return parent::render($element);
     }
 
+    /**
+     * Gets the AJAX URL
+     *
+     * @return string
+     */
     public function getAjaxUrl()
     {
         return $this->getUrl('doofinderfeed/integration/cleanIntegration');
     }
 
+    /**
+     * Gets the HTML markup for the "Reset All" button
+     *
+     * @return string
+     */
     public function getButtonHtml()
     {
-        $button = $this->getLayout()->createBlock('Magento\Backend\Block\Widget\Button')->setData([
+        $button = $this->getLayout()->createBlock(\Magento\Backend\Block\Widget\Button::class)->setData([
             'id' => 'clean-integration', 'label' => __('Reset All'),
         ]);
         return $button->toHtml();
     }
 
+    /**
+     * Gets the HTML markup for the current element
+     *
+     * @param AbstractElement $element
+     *
+     * @return string
+     */
     protected function _getElementHtml(AbstractElement $element)
     {
         return $this->_toHtml();
