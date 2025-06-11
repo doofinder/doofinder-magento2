2e7b3143210718029a457bfcf669e32374025e3c
davidmolinacano <davidmolinacano@gmail.com>
2025-06-11 12:14:44 +0200

    fix: Solved incorrect URL handling in product redirects

--- Doofinder/Feed/Helper/Product.php
+++ Doofinder/Feed/Helper/Product.php
@@ -162,6 +162,7 @@ class Product extends AbstractHelper
             UrlRewrite::ENTITY_ID => $productId,
             UrlRewrite::ENTITY_TYPE => ProductUrlRewriteGenerator::ENTITY_TYPE,
             UrlRewrite::STORE_ID => $storeId,
+            UrlRewrite::REDIRECT_TYPE => 0,
         ];
         $rewrite = $this->urlFinder->findOneByData($filterData);
         if ($rewrite) {
