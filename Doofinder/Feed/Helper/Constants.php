3f026e835753738bddac94462dc55e44d8d83dc8
Héctor <hector.acosta.valido@outlook.es>
2026-03-16 17:02:15 +0000

    removes the indexation status callback and enables the doofinder script by default

--- Doofinder/Feed/Helper/Constants.php
+++ Doofinder/Feed/Helper/Constants.php
@@ -6,9 +6,9 @@ namespace Doofinder\Feed\Helper;
 
 class Constants
 {
-    public const DOOFINDER_ADMIN_URL = 'https://admin.doofinder.com';
-    public const DOOFINDER_PLUGINS_URL_FORMAT = 'https://%s-plugins.doofinder.com';
-    public const DOOFINDER_SEARCH_URL_FORMAT = 'https://%s-search.doofinder.com';
-    public const DOOFINDER_API_URL_FORMAT = 'https://%s-api.doofinder.com';
+    public const DOOFINDER_ADMIN_URL = 'https://acova-doomanager.ngrok.doofinder.com';
+    public const DOOFINDER_PLUGINS_URL_FORMAT = 'https://acova-dooplugins.ngrok.doofinder.com';
+    public const DOOFINDER_SEARCH_URL_FORMAT = 'https://acova-search.ngrok.doofinder.com';
+    public const DOOFINDER_API_URL_FORMAT = 'https://acova-api.ngrok.doofinder.com';
     public const DOOFINDER_SCRIPT_URL_FORMAT = 'https://%s-config.doofinder.com/2.x/%s.js';
 }
